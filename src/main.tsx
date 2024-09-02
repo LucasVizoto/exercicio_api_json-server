a75bc47c8ee9b6dc7b2d48931802dca60c0e73f2
Lucas Vizoto <lucasvizoto364@gmail.com>
2024-09-02 20:59:08 -0300

    updating_trabalho

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+// https://github.com/LucasVizoto/exercicio_api_json-server
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
